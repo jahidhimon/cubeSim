247f660100c2bf9590f01a2840f2f3586229334d
jahdihimon <jahidhasanimonn@gmail.com>
2022-06-21 10:16:01 +0600

    Create just a window

--- main.c
+++ main.c
@@ -1,6 +1,29 @@
+#include <GL/freeglut_std.h>
+#include <GL/gl.h>
+#include <GL/glut.h>
+
+#include "main.h"
+
 #include <stdio.h>
 
+
+
 int main(int argc, char* argv[]) {
-	printf("hello world");
+	glutInit(&argc, argv);
+
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+	glutCreateWindow("cubeSim");
+
+	glutDisplayFunc(display);
+
+	glutMainLoop();
 	return 0;
+}
+
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glFlush();
+	glutSwapBuffers();
 }
---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define WINDOW_WIDTH  500
+#define WINDOW_HEIGHT 500
+
+#define TITLE "THE ROTATING CUBE"
+
+void display();
+#endif // MAIN_H
